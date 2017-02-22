668c5153a9077a354c60985f1bbd6f73508d6477
Kathy Joshi <kathyjoshi@yahoo.com>
2017-02-22 21:43:45 +0000

    added a variation

--- triangle_adv.py
+++ triangle_adv.py
@@ -1,3 +1,10 @@
-def advanced_nested_pyramid(start=1, last=5, char='*'):
-    pass
+def advanced_nested_triangle(start=1, last=5, char='*'):
+    box=""
+    for i in range(start, last+1):
+        line=""
+        for j in range(i):
+            line = ( char * (j+1))
+            line= line +"\n"
+        box =box+line
+    return box
     
