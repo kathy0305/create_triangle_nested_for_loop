beee5128a00fc3c2a740f1d6610e05ad3386995e
Kathy0305 <kathy0305@hotmail.com>
2017-02-22 16:46:51 -0500

    Update triple_nest.py

--- triple_nest.py
+++ triple_nest.py
@@ -1,2 +1,12 @@
 
-def triple ()
+def triple_nest(size=5):
+    box=""
+    for i in range(1,size+1):                   #big loop will start line (1-6)
+        line=""
+        for j in range (size-1):        #small loop to  add '.' reverse count
+            line=line +'.'
+        for m in range (i):             #small loop to add the string value of i
+            line=line+str(i)
+        line=line+"\n"                  #go back to big loop and add line break
+        box=box+line
+    return box
